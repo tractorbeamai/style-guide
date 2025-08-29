4ffa98c749bfbd51ea00a86cb639f8d4a4654613
wadefletch <wbfletch@gmail.com>
2025-08-29 17:53:00 -0500

    2025-08-29T22:53:00

--- eslint/index.ts
+++ eslint/index.ts
@@ -6,11 +6,7 @@ export * as browser from "./browser.js";
 export * as node from "./node.js";
 export * as vitest from "./vitest.js";
 
-// Typescript
-export * as typescript from "./typescript.js";
-
-// React
-export * as react from "./react.js";
-
-// Prettier
+// Tools
 export * as prettier from "./prettier.js";
+export * as react from "./react.js";
+export * as typescript from "./typescript.js";
